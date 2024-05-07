093b0ab14475adfe9dbdafe7ac707eab2d756c27
izuu1212 <165887212+izuu1212@users.noreply.github.com>
2024-05-07 18:38:15 +0700

    Update uts algoritma no.4.py

--- uts algoritma/uts algoritma no.4.py
+++ uts algoritma/uts algoritma no.4.py
@@ -12,18 +12,13 @@ def beri_rekomendasi(bmi):
     else:
         return "Obesitas. Anda perlu konsultasi dengan dokter untuk menurunkan berat badan."
 
-# Meminta pengguna untuk memasukkan berat badan (dalam kilogram)
 berat_badan = float(input("Masukkan berat badan Anda (kg): "))
 
-# Meminta pengguna untuk memasukkan tinggi badan (dalam meter)
 tinggi_badan = float(input("Masukkan tinggi badan Anda (m): "))
 
-# Menghitung BMI
 bmi = hitung_bmi(berat_badan, tinggi_badan)
 
-# Memberi rekomendasi berdasarkan BMI
 rekomendasi = beri_rekomendasi(bmi)
 
-# Menampilkan hasil
 print("BMI Anda adalah:", round(bmi, 2))
 print("Rekomendasi:", rekomendasi)
